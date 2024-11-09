9d84216b107c9394ec7d4203481a854a5c6bfe7b
ritik-kedia <ritikkedia039@gmail.com>
2024-11-09 13:54:36 +0530

    Refactored code to use serverless functions for Vercel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const authRoutes = require("./routes/auth")
+const authRoutes = require("./api/auth")
 
 const app = express();
 app.use(bodyParser.json());
